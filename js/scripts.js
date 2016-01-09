5a8e18f0142f130896a9bda4607f2597feeb5f16
Jim Strother <strotherwebdev@gmail.com>
2016-01-09 22:14:13 +0000

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    for (var i = 1; i <= 100; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            $('#fizzbuzz').append('FizzBuzz<br />');
+        } else if (i % 3 == 0) {
+            $('#fizzbuzz').append('Fizz<br />');
+        } else if (i % 5 == 0) {
+            $('#fizzbuzz').append('Buzz<br />');
+        } else {
+            $('#fizzbuzz').append(i + '<br />');
+        }
+        console.log(i);
+    }
+});
